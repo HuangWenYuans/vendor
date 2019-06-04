97b89dc555ef98dbcf227eb7f6bebcf7d7859254
S_16201213 <79868058@qq.com>
2019-06-04 23:55:23 +0800

    项目最初模板

---
+++ src/main/java/com/hwy/vendor/VendorApplication.java
@@ -0,0 +1,13 @@
+package com.hwy.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendorApplication.class, args);
+    }
+
+}
